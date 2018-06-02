d6bf1793c8678ffc51f1aed8e686863ef3d261f1
zzy131250 <zzy131250@outlook.com>
2018-06-02 11:33:04 +0800

    add content

--- src/base/EularPrimeSelect.java
+++ src/base/EularPrimeSelect.java
@@ -13,8 +13,11 @@ public class EularPrimeSelect {
 		for (int i = 2; i < n; i++) {
 			if (isPrime[i]) primes.add(i);
 			for (int j = 0; j < primes.size(); j++) {
+				// 越界，结束
 				if (i * primes.get(j) >= n) break;
+				// 素数的倍数都是合数
 				isPrime[i * primes.get(j)] = false;
+				// 基数是已知素数的倍数，结束；确保筛选时用的是最小质因数
 				if (i % primes.get(j) == 0) break;
 			}
 		}
--- src/scan/MaxSubArray.java
+++ src/scan/MaxSubArray.java
@@ -1,4 +1,23 @@
 package scan;
 
+/**
+ * 连续子数组最大值
+ * 最优方法，时间复杂度O（n）
+ * 和最大的子序列的第一个元素肯定是正数
+ * 因为元素有正有负，因此子序列的最大和一定大于0
+ */
 public class MaxSubArray {
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, -2, 3, 10, -4, 7, 2, -5};
+        int max = 0;
+        int currentSum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            currentSum += arr[i];
+            if (currentSum > max) max = currentSum;
+            if (currentSum < 0) currentSum = 0;
+        }
+        System.out.println(max);
+    }
+
 }
